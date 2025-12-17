aed0e476266b11ed51e9a723cac6b3830536efe0
Ilyaf <ilyaf@radware.com>
2025-12-17 16:49:46 +0200

    1

--- Dusklight/frontend/vite.config.js
+++ Dusklight/frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+server: {
+  proxy: {
+    '/': {
+      target: 'http://127.0.0.1:8000',
+      changeOrigin: true,
+      // Optionally, add a bypass for static files if needed
+    },
+  },
+},
 })
